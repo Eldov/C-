57161f6c41fc7e0cecd4f057133563c298c1037f
Emily Veiga <emili_veiga@yahoo.com.br>
2022-02-27 18:46:42 +0000

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Pasta Lógica
+namespace HelloWorld
+{
+    class Program
+    {
+        //Isso é um comentário, uma linha! Tudo que for escrito será ignorado pelo compilador
+        /*Este
+         é um
+        comentário de
+        mais de uma linha*/
+        //Método Main
+        //Ponto de entrada da aplicação, ou seja, é o que define o início da aplicação
+        static void Main(string[] args)
+        {
+            //Imprimir na tela o famoso "Olá, Mundo!"
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Meu nome é Emily");
+        }
+    }
+}
